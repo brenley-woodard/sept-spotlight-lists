65ef69cb696c8a3b306c06b2ea4da05b29519ac8
brenley-woodard <brenleyew@gmail.com>
2022-09-15 12:01:12 -0700

    exported variable to hold arrays of candy items and gave ID's to HTML list elements

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
---
+++ candies-data.js
@@ -0,0 +1,17 @@
+export const candy [
+    {
+        name: 'Snickers',
+        flavor: 'chocolate caramel peanut',
+        type: 'candy bar'
+    }
+    {
+        name: 'Hi-Chews',
+        flavor: 'fruity ',
+        type: 'gummy bites'
+    }
+    {
+        name: 'Nerds',
+        flavor: 'sweet & tart',
+        type: 'crunchy pebbles'
+    }
+]
